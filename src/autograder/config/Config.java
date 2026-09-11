227b33e1ade60d85e1f1374136255c05b2c74a4d
MarcoEditia <marcoeditia@gmail.com>
2026-09-11 23:11:27 +0800

    add functions that load Path. getTesterFilesPath and getStudentSubmissionPath

--- src/autograder/config/Config.java
+++ src/autograder/config/Config.java
@@ -2,13 +2,14 @@ package autograder.config;
 
 import java.util.Properties;
 import java.io.InputStream;
+import java.nio.file.Path;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
 public class Config {
-    private final Properties properties = new Properties();
+    private final Properties configProperties = new Properties();
 
     public Config(){
         loadConfiguration();
@@ -21,12 +22,25 @@ public class Config {
             if(is == null){
                 throw new FileNotFoundException("Could not find " + configPropertiesFileName + "in autograder.config");
             }
-            properties.load(is);
+            configProperties.load(is);
+        
         } catch (IOException error){
             throw new IllegalStateException("Failed to load config.properties", error);
         }
     }
 
-    public get
+    public Path getTesterFilesPath(){
+        String rawTesterFilesPath = configProperties.getProperty("TESTER_FILES_PATH").trim();
+        Path TesterFilesPath = Path.of(rawTesterFilesPath).toAbsolutePath().normalize();
+
+        return TesterFilesPath;
+    }
+
+    public Path getStudentSubmissionPath(){
+        String rawStudentSubmissionPath = configProperties.getProperty("STUDENT_SUBMISSION_PATH").trim();
+        Path studentSubmissionPath = Path.of(rawStudentSubmissionPath).toAbsolutePath().normalize();
+
+        return studentSubmissionPath;
+    }
     
 }
